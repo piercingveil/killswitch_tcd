6d2fb10461957c6f8ddd60b16368c71ae5288d60
syra <89045255+piercingveil@users.noreply.github.com>
2025-11-18 10:37:32 -0500

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,3 +1,4 @@
 @echo off
-cmd /c start "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "D:\killswitch_tcd\pxl.ps1"
+cmd /c start "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "E:\killswitch_tcd\pxl.ps1"
 exit
+
